eb60a80526a008a79ff9ea995e7a852fd7012530
Amari-sara2001 <96389043+Amari-sara2001@users.noreply.github.com>
2022-12-03 23:49:34 +0100

    solution_Qes03

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,14 +4,14 @@ import java.sql.SQLException;
 
 public class DBConnection {
 	   
-		String BDD = "nomBD";
-		String url = "jdbc:postgresql://localhost:5432/postgres";
+		String BDD = "tparchi";
+		String url = "jdbc:postgresql://localhost:5432/postgres" +BDD;
 		String user = "postgres";
 		String passwd = "amarisara";
 	    private Connection conn;
 
-	   
-	    public DBConnection() throws SQLException {
+	    private static DBConnection instance;
+	    private DBConnection() throws SQLException {
 			conn=DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","amarisara");
 		}
 
@@ -19,6 +19,12 @@ public class DBConnection {
 	    public Connection getConn() {
 			return conn;
 		}
+	    public static DBConnection getInstance() throws SQLException {
+	    	if(instance == null) {
+	    		instance = new DBConnection();
+	    	}
+	    	return instance;
+	    }
 
 
 		
